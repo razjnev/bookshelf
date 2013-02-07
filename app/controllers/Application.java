de8712c6a3b6ffe4c0148791df5574389fa9704d
Caroline <corr@nelsonthornes.com>
2013-02-07 16:41:42 +0000

    adding empty bookshelf implementation

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,23 @@
 package controllers;
 
+import models.bookshelf.Bookshelf;
+import org.codehaus.jackson.node.ObjectNode;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 
 public class Application extends Controller {
-  
+
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
-  
+
+    public static Result bookshelf() {
+        ObjectNode result = Json.newObject();
+        result.put("success", true);
+        result.put("bookshelf", Json.toJson(new Bookshelf()));
+        return ok(result);
+    }
+
 }
---
+++ app/models/bookshelf/Bookshelf.java
@@ -0,0 +1,16 @@
+package models.bookshelf;
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Describe this class.
+ *
+ * @author Caroline
+ * @since 07/02/13
+ */
+public class Bookshelf {
+
+    public List<Book> books = new ArrayList<>();
+}
